fa06aba6761f398947defee58eb75a0d25ac6ab0
aderawmolla <mollaaderaw@gmail.com>
2023-05-05 17:25:03 +0300

    initialize project

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <h1 className=" rounded-xl border-2  border-green-400  text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
